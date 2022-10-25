ce21be99f797b1d8ef52ecb89723aa5108493743
Aurimas Bruzgys <55162517+Ranteidz@users.noreply.github.com>
2022-10-25 15:32:18 +0200

    Imported coin assets, added coin collecting

---
+++ Assignmnent3/Assets/_Scripts/CollectCoin.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoin : MonoBehaviour
+{
+    public int coins;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            coins = coins + 1;
+            other.gameObject.SetActive(false);
+            Destroy(other.gameObject);
+        }
+    }
+}
